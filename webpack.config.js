be08290471beb98caf53eac8689da80ac8941955
NewsCartDev <newscart.dev@gmail.com>
2017-07-31 15:05:56 -0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const config = require('config');
+
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:' + config.get('devServer.port') ,
+    path.resolve(__dirname, './src/index.js')
+  ],
+  output: {
+    filename: '[name]-[hash].js',
+    path: path.resolve(__dirname, './dist'),
+    publicPath: ''
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './index.html'),
+      inject: {
+        body: true
+      }
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.elm$/,
+        use: ['elm-webpack-loader']
+      }
+    ]
+  },
+  devServer: {
+    port: config.get('devServer.port')
+  }
+}
